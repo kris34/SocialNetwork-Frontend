8751d2697f07f48ae46d1c894e40dd02ca993a89
Kristian Lalov <lalov.kr@gmail.com>
2023-08-21 23:09:00 +0300

    add register component and auth routes

---
+++ src/app/user/components/register/register.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'sc-register',
+  templateUrl: './register.component.html',
+  standalone: true,
+})
+export class RegisterComponent {}
