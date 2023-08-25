8433ab11facb6b5be9d5a9692a2a4caafe4c46ae
Kristian Lalov <lalov.kr@gmail.com>
2023-08-25 20:45:39 +0300

    added register reducer

---
+++ src/app/shared/types/currentUserInterface.ts
@@ -0,0 +1,8 @@
+export interface CurrentUserInterface { 
+    username: string;
+    email: string;
+    _id: string;
+    accessToken: string;
+    friendRequests: string[];
+    friends: string[]
+}
