6b119dcb76c84de6324661dece16cf4746728ccf
Kristian Lalov <lalov.kr@gmail.com>
2023-09-05 17:30:24 +0300

    add fontawesome to project

---
+++ src/app/shared/components/topBar/topBar.component.ts
@@ -0,0 +1,23 @@
+import {CommonModule} from '@angular/common'
+import {Component} from '@angular/core'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import {Store} from '@ngrx/store'
+import {combineLatest} from 'rxjs'
+import {selectCurrentUser} from 'src/app/user/store/reducers'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+
+@Component({
+  selector: 'mc-topbar',
+  templateUrl: './topBar.component.html',
+  standalone: true,
+  imports: [CommonModule, FontAwesomeModule ]
+})
+export class TopBarComponent {
+  data$ = combineLatest({
+    currentUser: this.store.select(selectCurrentUser),
+  })
+
+  home = faHome
+
+  constructor(private store: Store) {}
+}
