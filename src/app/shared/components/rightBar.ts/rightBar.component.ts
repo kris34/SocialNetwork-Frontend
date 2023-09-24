b5b6d4714731423b0381221c57a62fb1bf87b3e0
Kristian Lalov <lalov.kr@gmail.com>
2023-09-24 14:53:42 +0300

    add inbox button right bar

--- src/app/shared/components/rightBar.ts/rightBar.component.ts
+++ src/app/shared/components/rightBar.ts/rightBar.component.ts
@@ -2,7 +2,7 @@ import {CommonModule} from '@angular/common'
 import {Component} from '@angular/core'
 import { RouterLink } from '@angular/router'
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
-import {faUserGroup} from '@fortawesome/free-solid-svg-icons'
+import {faInbox, faUserGroup} from '@fortawesome/free-solid-svg-icons'
 import {Store} from '@ngrx/store'
 import {combineLatest} from 'rxjs'
 import {LoginComponent} from 'src/app/user/components/login/login.component'
@@ -16,6 +16,7 @@ import {selectCurrentUser} from 'src/app/user/store/reducers'
 })
 export class RightBarComponent {
   friendsIcon = faUserGroup
+  inboxIcon = faInbox
 
   data$ = combineLatest({
     currentUser: this.store.select(selectCurrentUser),
